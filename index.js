33f3f65a5308149208f2e1c14207c441ece02bb6
Maryam Verdon <maryamverdon@gmail.com>
2022-09-10 18:58:40 +0200

    affichage des plats

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+const result = document.getElementById("result");
+const form = document.querySelector("form");
+let meals = [];
 
+const fetchMeals = async () => {
+  await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken")
+    .then((res) => res.json())
+    .then((data) => (meals = data.meals));
+
+  console.log(meals);
+};
+
+const mealsDisplay = () => {
+  meals.length = 12;
+  result.innerHTML = meals
+    .map(
+      (meal) =>
+        `
+      <li class="card">
+        <h2>${meal.strMeal}</h2>
+        <p>${meal.strArea}</p>
+        <img src=${meal.strMealThumb} alt="photo ${meal.sttrMeal}">
+        <ul></ul>
+      </li>
+     `
+    )
+    .join("");
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetchMeals().then(() => mealsDisplay());
+});
