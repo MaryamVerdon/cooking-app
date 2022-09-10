554ed1d485b8e35bb0d99cca5311d5ba5b89bca1
Maryam Verdon <maryamverdon@gmail.com>
2022-09-10 19:22:08 +0200

    affichage des ingrédients et quantités des recettes

--- index.js
+++ index.js
@@ -12,20 +12,31 @@ const fetchMeals = async (search) => {
 };
 
 const mealsDisplay = () => {
-  meals.length = 12;
-  result.innerHTML = meals
-    .map(
-      (meal) =>
-        `
-      <li class="card">
-        <h2>${meal.strMeal}</h2>
-        <p>${meal.strArea}</p>
-        <img src=${meal.strMealThumb} alt="photo ${meal.sttrMeal}">
-        <ul></ul>
-      </li>
-     `
-    )
-    .join("");
+  if (meals === null) {
+    result.innerHTML = "<h2>Aucun résultat</h2>";
+  } else {
+    meals.length = 12;
+    result.innerHTML = meals
+      .map((meal) => {
+        let ingredients = [];
+        for (let i = 1; i < 21; i++) {
+          if (meal[`strIngredient${i}`]) {
+            let ingredient = meal[`strIngredient${i}`];
+            let measure = meal[`strMeasure${i}`];
+            ingredients.push("<li>" + ingredient + " - " + measure + "</li>");
+          }
+        }
+        return `
+            <li class="card">
+            <h2>${meal.strMeal}</h2>
+            <p>${meal.strArea}</p>
+            <img src=${meal.strMealThumb} alt="photo ${meal.sttrMeal}">
+            <ul>${ingredients.join("")}</ul>
+            </li>
+            `;
+      })
+      .join("");
+  }
 };
 
 input.addEventListener("input", (e) => {
